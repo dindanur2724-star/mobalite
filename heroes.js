351a990a51fa99e6b6257192d58be1fedd5bea99
dindanur2724-star <dinda.nur2724@smk.belajar.id>
2026-02-19 10:42:07 +0700

    Add HEROES array with hero attributes

---
+++ heroes.js
@@ -0,0 +1,23 @@
+const HEROES = [
+  {
+    name: "Flora",
+    color: "#22c55e",
+    hp: 130,
+    damage: 3,
+    skill: "heal"
+  },
+  {
+    name: "Blaze",
+    color: "#fb923c",
+    hp: 100,
+    damage: 5,
+    skill: "fire"
+  },
+  {
+    name: "Shade",
+    color: "#8b5cf6",
+    hp: 90,
+    damage: 6,
+    skill: "dash"
+  }
+];
